2677b9814016b251a373956eb480fe114b74e86f
trungduong0103 <trungduong0103@gmail.com>
2020-07-14 15:06:21 +0700

    add tabs with titles

--- App.js
+++ App.js
@@ -1,112 +1,24 @@
-import * as React from 'react';
+import React from 'react';
 import 'react-native-gesture-handler';
-import {Text, View, TouchableOpacity} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+
+import HomeStackScreens from './src/routes/home';
+import DashboardStackScreens from './src/routes/dashboard';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-
-function HomeScreen() {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
-
-function MyTabBar({state, descriptors, navigation}) {
-  const focusedOptions = descriptors[state.routes[state.index].key].options;
-
-  if (focusedOptions.tabBarVisible === false) {
-    return null;
-  }
-
-  return (
-    <View style={{flexDirection: 'row'}}>
-      {state.routes.map((route, index) => {
-        const {options} = descriptors[route.key];
-        const label =
-          options.tabBarLabel !== undefined
-            ? options.tabBarLabel
-            : options.title !== undefined
-            ? options.title
-            : route.name;
-
-        const isFocused = state.index === index;
-
-        const onPress = () => {
-          const event = navigation.emit({
-            type: 'tabPress',
-            target: route.key,
-            canPreventDefault: true,
-          });
-
-          if (!isFocused && !event.defaultPrevented) {
-            navigation.navigate(route.name);
-          }
-        };
-
-        const onLongPress = () => {
-          navigation.emit({
-            type: 'tabLongPress',
-            target: route.key,
-          });
-        };
-
-        return (
-          <TouchableOpacity
-            accessibilityRole="button"
-            accessibilityStates={isFocused ? ['selected'] : []}
-            accessibilityLabel={options.tabBarAccessibilityLabel}
-            testID={options.tabBarTestID}
-            onPress={onPress}
-            onLongPress={onLongPress}
-            style={{flex: 1, height: 100}}
-            key={index}>
-            <Text style={{color: isFocused ? '#673ab7' : '#222'}}>{label}</Text>
-          </TouchableOpacity>
-        );
-      })}
-    </View>
-  );
-}
+import ROUTES from './src/routes/constants';
+import TabWithIcon from './src/routes/tab-with-icon';
 
 const Tab = createBottomTabNavigator();
 
-export default function App() {
+const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator tabBar={props => <MyTabBar {...props} />}>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Navigator tabBar={props => <TabWithIcon {...props} />}>
+        <Tab.Screen name={ROUTES.HOME} component={HomeStackScreens} />
+        <Tab.Screen name={ROUTES.DASHBOARD} component={DashboardStackScreens} />
       </Tab.Navigator>
     </NavigationContainer>
   );
-}
+};
 
-// import React from 'react';
-// import {NavigationContainer} from '@react-navigation/native';
-// import {createStackNavigator} from '@react-navigation/stack';
-//
-// import MainPage from './src/views';
-// import ROUTES from './src/routes/constants';
-//
-// const MainStack = createStackNavigator();
-//
-// const App = () => {
-//   return (
-//     <NavigationContainer>
-//       <MainStack.Navigator>
-//         <MainStack.Screen name={ROUTES.MAIN} component={MainPage} />
-//       </MainStack.Navigator>
-//     </NavigationContainer>
-//   )
-// };
-//
-// export default App;
+export default App;
