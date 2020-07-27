393362c712e0533ceb2a010264d93e08d1a7d284
trungduong0103 <trungduong0103@gmail.com>
2020-07-27 18:30:33 +0700

    detect barcode and add a region

--- src/components/camera/Camera.js
+++ src/components/camera/Camera.js
@@ -1,72 +1,72 @@
-import React from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text} from 'react-native';
 import {RNCamera} from 'react-native-camera';
 
 const Camera = () => {
-  const takePicture = async () => {
-    if (this.camera) {
-      const options = {quality: 0.5, base64: true};
-      const data = await this.camera.takePictureAsync(options);
-      console.log(data.uri);
-    }
+  const [barcode, setBarCode] = useState(null);
+
+  const onBarCodeRecognized = barcodeScanned => {
+    setBarCode(barcodeScanned);
+    console.log(barcodeScanned.bounds.size)
   };
 
-  const onBarcodeScan = (barcode) => {
-    console.log(barcode);
-  };
+  const renderBarcode = (bounds, data) => (
+    <React.Fragment>
+      <View
+        style={{
+          borderWidth: 2,
+          borderRadius: 10,
+          position: 'absolute',
+          borderColor: '#F00',
+          justifyContent: 'center',
+          backgroundColor: 'rgba(255, 255, 255, 0.9)',
+          padding: 10,
+          height: parseInt(bounds.size.height),
+          width: parseInt(bounds.size.width),
+          left: parseInt(bounds.origin.x, 10),
+          top: parseInt(bounds.origin.y, 10),
+        }}>
+        <Text
+          style={{
+            color: '#F00',
+            flex: 1,
+            position: 'absolute',
+            textAlign: 'center',
+            backgroundColor: 'transparent',
+          }}>
+          {data}
+        </Text>
+      </View>
+    </React.Fragment>
+  );
 
   return (
-    <View style={styles.container}>
-      <RNCamera
-        ref={ref => {
-          this.camera = ref;
-        }}
-        style={styles.preview}
-        type={RNCamera.Constants.Type.back}
-        flashMode={RNCamera.Constants.FlashMode.on}
-        androidCameraPermissionOptions={{
-          title: 'Permission to use camera',
-          message: 'We need your permission to use your camera',
-          buttonPositive: 'Ok',
-          buttonNegative: 'Cancel',
-        }}
-        androidRecordAudioPermissionOptions={{
-          title: 'Permission to use audio recording',
-          message: 'We need your permission to use your audio',
-          buttonPositive: 'Ok',
-          buttonNegative: 'Cancel',
-        }}
-        onBarCodeRead={onBarcodeScan}
-      />
-      <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
-        <TouchableOpacity onPress={() => takePicture()} style={styles.capture}>
-          <Text style={{fontSize: 14}}> SNAP </Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+    <RNCamera
+      style={{
+        flex: 1,
+        width: '100%',
+      }}
+      ref={ref => {
+        this.camera = ref;
+      }}
+      type={RNCamera.Constants.Type.back}
+      flashMode={RNCamera.Constants.FlashMode.on}
+      androidCameraPermissionOptions={{
+        title: 'Permission to use camera',
+        message: 'We need your permission to use your camera',
+        buttonPositive: 'Ok',
+        buttonNegative: 'Cancel',
+      }}
+      androidRecordAudioPermissionOptions={{
+        title: 'Permission to use audio recording',
+        message: 'We need your permission to use your audio',
+        buttonPositive: 'Ok',
+        buttonNegative: 'Cancel',
+      }}
+      onBarCodeRead={onBarCodeRecognized}>
+      {barcode && <View>{renderBarcode(barcode.bounds, barcode.data)}</View>}
+    </RNCamera>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black',
-  },
-  preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    padding: 15,
-    paddingHorizontal: 20,
-    alignSelf: 'center',
-    margin: 20,
-  },
-});
 
 export default Camera;
