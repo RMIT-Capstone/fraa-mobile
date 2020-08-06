be4d06e8f0d8283b8aed40d4564c6a9eb58f2aa9
trungduong0103 <trungduong0103@gmail.com>
2020-08-06 19:27:53 +0700

    WIP: get user location

--- src/components/check-in/front-face-camera/FrontFaceCamera.js
+++ src/components/check-in/front-face-camera/FrontFaceCamera.js
@@ -1,29 +1,17 @@
 import React, {useState} from 'react';
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  ImageBackground,
-  StyleSheet,
-} from 'react-native';
+import {ImageBackground, Text, TouchableOpacity, View} from 'react-native';
+import styles from './FrontFaceCameraStyle';
 import {RNCamera} from 'react-native-camera';
-import theme from '../../../theme';
+import RNLocation from 'react-native-location';
+import {getDistanceFromLatLonInKm} from '../../../helpers/utils';
+import LottieView from 'lottie-react-native';
+const GenericLoading = require('../../../lottie-assets/GenericLoading');
 
 const FrontFaceCamera = () => {
   const [recognizedFaces, setRecognizedFaces] = useState([]);
   const [previewImage, setPreviewImage] = useState(null);
-
-  const takePicture = async camera => {
-    const options = {quality: 0.5, base64: true};
-    try {
-      const data = await camera.takePictureAsync(options);
-      if (data) {
-        setPreviewImage(data.uri);
-      }
-    } catch (errorCapture) {
-      console.warn(errorCapture);
-    }
-  };
+  const [loading, setLoading] = useState(false);
+  const [tooFar, setTooFar] = useState(false);
 
   const onFaceDetected = ({faces}) => {
     if (faces) {
@@ -33,6 +21,60 @@ const FrontFaceCamera = () => {
     }
   };
 
+  const takePicture = async camera => {
+    const options = {quality: 0.5};
+    setLoading(true);
+    try {
+      const data = await camera.takePictureAsync(options);
+      if (data) {
+        setPreviewImage(data.uri);
+        setLoading(false);
+        await getLocation();
+      }
+    } catch (errorCapture) {
+      console.warn(errorCapture);
+    }
+  };
+
+  const getLocation = async () => {
+    await RNLocation.configure({
+      distanceFilter: 1.0,
+      desiredAccuracy: {
+        ios: 'best',
+        android: 'balancedPowerAccuracy',
+      },
+    });
+
+    RNLocation.requestPermission({
+      ios: 'whenInUse',
+      android: {
+        detail: 'fine',
+      },
+    }).then(granted => {
+      if (granted) {
+        RNLocation.subscribeToLocationUpdates(locations => {
+          // replace this to test location
+          const lat = 10.729239078509787;
+          const lng = 106.6965148240102;
+          locations.forEach(location => {
+            if (
+              getDistanceFromLatLonInKm(
+                lat,
+                lng,
+                location.latitude,
+                location.longitude,
+              ) >= 10
+            ) {
+              setTooFar(true);
+            } else {
+              setTooFar(false);
+            }
+          });
+        });
+      }
+    });
+  };
+
   const renderFaceBounds = () => {
     return recognizedFaces.map((face, index) => (
       <View
@@ -40,10 +82,10 @@ const FrontFaceCamera = () => {
         style={[
           styles.faceBounds,
           {
-            height: parseInt(face.bounds.size.height, 10),
-            width: parseInt(face.bounds.size.width, 10),
-            left: parseInt(face.bounds.origin.x, 10),
-            top: parseInt(face.bounds.origin.y, 10),
+            height: parseFloat(face.bounds.size.height),
+            width: parseFloat(face.bounds.size.width),
+            left: parseFloat(face.bounds.origin.x),
+            top: parseFloat(face.bounds.origin.y),
           },
         ]}
       />
@@ -98,13 +140,20 @@ const FrontFaceCamera = () => {
             <>
               {renderFaceBounds()}
               {recognizedFaces.length === 1 && (
-                <View style={styles.snapButton}>
-                  <TouchableOpacity
-                    onPress={() => takePicture(camera)}
-                    style={styles.capture}>
+                <TouchableOpacity
+                  onPress={() => takePicture(camera)}
+                  style={styles.snapButton}>
+                  {loading ? (
+                    <LottieView
+                      source={GenericLoading}
+                      autoPlay
+                      loop
+                      style={styles.lottieView}
+                    />
+                  ) : (
                     <Text style={styles.snapText}>Snap</Text>
-                  </TouchableOpacity>
-                </View>
+                  )}
+                </TouchableOpacity>
               )}
               {recognizedFaces.length > 1 && (
                 <View style={styles.tooManyFacesView}>
@@ -120,41 +169,5 @@ const FrontFaceCamera = () => {
     </RNCamera>
   );
 };
-
-const styles = StyleSheet.create({
-  camera: {
-    flex: 1,
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  snapButton: {
-    position: 'absolute',
-    bottom: 20,
-    backgroundColor: theme.palette.primary.red,
-    padding: 15,
-    borderRadius: 16,
-  },
-  snapText: {
-    fontSize: 14,
-    color: '#fff',
-    fontWeight: '700',
-  },
-  faceBounds: {
-    borderWidth: 1.5,
-    position: 'absolute',
-    borderColor: theme.palette.secondary.yellow,
-  },
-  tooManyFacesView: {
-    backgroundColor: theme.palette.primary.red,
-    padding: 15,
-    borderRadius: 10,
-  },
-  tooManyFacesText: {
-    color: '#fff',
-    fontSize: 14,
-    fontWeight: '700',
-  },
-});
 
 export default FrontFaceCamera;
