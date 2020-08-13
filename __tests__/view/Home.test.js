d40972ccd8266243982c661e486d1ee87bf16d9c
pham-hang <phamngocminhhang@gmail.com>
2020-08-13 11:03:40 +0700

    Modified snapshot

--- __tests__/view/Home.test.js
+++ __tests__/view/Home.test.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Home from '../../src/views/home/Home';
+
 test('renders Home correctly', () => {
   const navigation = {};
   const tree = renderer.create(<Home navigation={navigation} />).toJSON();
